595cf879afbed9806f250a5ba7f11af529e8cdcf
EmberFox <161511358+EmberFox1221@users.noreply.github.com>
2025-11-17 01:36:34 -0600

    Update abx-functions.hpp

--- abx-functions.hpp
+++ abx-functions.hpp
@@ -19,6 +19,28 @@
 // based on https://www.cclsolutionsgroup.com/post/android-abx-binary-xml
 
 using namespace std;
+static const std::string b64_chars =
+             "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+             "abcdefghijklmnopqrstuvwxyz"
+             "0123456789+/";
+
+static std::string simple_base64(const std::string &in) {
+    std::string out;
+    int val = 0, valb = -6;
+    for (unsigned char c : in) {
+        val = (val << 8) + c;
+        valb += 8;
+        while (valb >= 0) {
+            out.push_back(b64_chars[(val >> valb) & 0x3F]);
+            valb -= 6;
+        }
+    }
+    if (valb > -6)
+        out.push_back(b64_chars[((val << 8) >> (valb + 8)) & 0x3F]);
+    while (out.size() % 4)
+        out.push_back('=');
+    return out;
+}
 
 class AbxToXml
 {
