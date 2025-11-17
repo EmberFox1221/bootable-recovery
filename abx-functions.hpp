4a683e8a6b3ea15c5b68188be62d2a8424a735b0
EmberFox <161511358+EmberFox1221@users.noreply.github.com>
2025-11-17 00:33:39 -0600

    Refactor base64 encoding in abx-functions.hpp
    Refactored base64 encoding function to improve clarity and removed redundant code.

--- abx-functions.hpp
+++ abx-functions.hpp
@@ -226,31 +226,27 @@ private:
 					std::string b64;
 					auto outlen = ((s.length() + 2) / 3) * 4;
 					b64.resize(outlen+1);		// +1 for null terminator
-					static const char b64_table[] =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+					static const char b64_table[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+                    std::string encode_base64(const std::string &in) {
+                      std::string out;
+                      int val = 0, valb = -6;
+                      for (unsigned char c : in) {
+                        val = (val << 8) + c;
+                        valb += 8;
+                        while (valb >= 0) {
+                          out.push_back(b64_table[(val >> valb) & 0x3F]);
+                          valb -= 6;
+                       }
+                    }
+                    if (valb > -6)
+                      out.push_back(b64_table[((val << 8) >> (valb + 8)) & 0x3F]);
+                      while (out.size() % 4)
+                      out.push_back('=');
+                      return out;
+                   }
 
-std::string encode_base64(const std::string &in)
-{
-    std::string out;
-    int val = 0, valb = -6;
-
-    for (unsigned char c : in) {
-        val = (val << 8) + c;
-        valb += 8;
-        while (valb >= 0) {
-            out.push_back(b64_table[(val >> valb) & 0x3F]);
-            valb -= 6;
-        }
-    }
-    if (valb > -6)
-        out.push_back(b64_table[((val << 8) >> (valb + 8)) & 0x3F]);
-    while (out.size() % 4)
-        out.push_back('=');
-    return out;
-}
-
-// Replace the EVP call:
-std::string b64 = encode_base64(s);
+                     // Replace the EVP call:
+                     std::string b64 = encode_base64(s);
 
 					if (got != outlen)
 					{
